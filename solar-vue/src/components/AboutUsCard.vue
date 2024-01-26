1e9d87d2c2bc2a40abb0d7d373dd9a0371c4d17a
Matthias Lücke <matthias.luecke@d2c-recommerce.com>
2024-01-26 18:57:55 +0100

    add comp about us card

---
+++ solar-vue/src/components/AboutUsCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="about-us-card">
+    <q-card class="q-ma-md q-pa-sm">
+      <h4 class="q-mb-md">{{ title }}</h4>
+      <p class="q-ma-xs">
+        {{ text }}
+      </p>
+    </q-card>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'AboutUsCard',
+  components: {},
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ solar-vue/src/utils/aboutUsTexts.ts
@@ -0,0 +1,45 @@
+export const aboutUsTexts = [
+  {
+    title: 'Alles aus einer Hand',
+    text: `
+        Alle Arbeiten, die im Zuge der Installation einer Photovoltaik-Anlage
+        anfallen, machen wir selber: von der Anfrage, dem ersten Kontakt und der
+        Planung über die Anmeldung beim Netzbetreiber bis hin zur Montage auf
+        dem Dach und dem elektrischen Anschluss gibt es bei uns alles aus einer
+        Hand. Dabei geben wir keine Teil-Arbeiten an sogenannte Subunternehmer
+        weiter. Lediglich das Aufstellen des Gerüsts wird von uns in der Regel
+        beauftragt. Das hat den Vorteil, dass die Menschen, für die wir die
+        PV-Anlagen bauen, einen Ansprechpartner haben und sich sicher sein
+        können, wer die Verantwortung für die korrekte Installation trägt.
+        Außerdem gibt es maximale Transparenz, was die Zusammensetzung des
+        Preises und die Aufteilung der Kosten auf Material und Arbeitszeit
+        betrifft.
+        `,
+  },
+  {
+    title: 'kollektiv',
+    text: `
+        Bei uns gibt es keine Investoren oder Kreditgeber, für die wir Gewinne erwirtschaften müssen. Der
+        Betrieb gehört uns als Belegschaft und damit den Menschen, die die tägliche Arbeit verrichten. Wir
+        entscheiden gemeinsam und gleichberechtigt darüber, wie sich der Betrieb gestaltet wird und wie
+        wir arbeiten wollen. Damit entscheiden also die Menschen über die Arbeitsbedingungen, die davon
+        auch direkt betroffen sind und nicht irgendwelche Gesellschafter:innen in der der Ferne',
+    `,
+  },
+  {
+    title: 'Ökologisch',
+    text: `
+        Wir wollen einen Beitrag dazu leisten, dass die Erderwärmung & die Umweltzerstörung infolge des
+        Klimawandels in Grenzen gehalten werden. Das wollen wir nicht nur durch die Installation von
+        Photovoltaik-Anlagen als umweltfreundliche Alternative zur fossilen Energieerzeugung erreichen,
+        sondern auch durch ein umweltfreundliches Agieren unseres Betriebs selber.
+        Daher haben wir kein Betriebs-Auto und versuchen, möglichst viele Fahrten zu den Baustellen mit
+        öffentlichen Verkehrsmitteln und Fahrrad zu erledigen. Um Material zu transportieren, nutzen wir
+        unseren betriebseigenen Fahrrad-Anhänger. Für größere Transporte nutzen wir einen elektrischen
+        Lasten-Anhänger für’s Fahrrad oder greifen – falls es nicht anders geht – auf ein Leihauto zurück.
+        Um unseren ökologischen Fußabdruck als Betrieb möglichst gering zu halten, beziehen wir
+        außerdem für unser Büro Ökostrom von den Bürgerwerken, der zu 100% aus bürgereigenen
+        Erneuerbare-Energien-Anlagen stammt.
+    `,
+  },
+];
